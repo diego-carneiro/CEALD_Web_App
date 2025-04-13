93469a4b01244f5c3bab4acf313aa9e3fd939e2c
Diego Carneiro <diego-linux@Acer-Diego>
2025-04-13 09:34:47 -0300

    router fixing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,13 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 /*::: Pages :::*/
 import GuestPage from "@/pages/GuestPage";
 import ADM from "@/pages/ADM";
 
 export const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/retirar-senha" replace />,
+  },
   {
     path: "/retirar-senha",
     element: <GuestPage />, // Retirada de senhas
